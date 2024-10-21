0a0ad55e05b746a8f7de3fc7b4409a7b0c6020c7
kimutai <kevinkogo@gmail.com>
2024-10-22 00:09:24 +0300

    Added Django Debug  Environ Variable

--- src/SaaSApp/settings.py
+++ src/SaaSApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qh(ikiqv668v#78n5nk1o73qkbh6!_6h__w$g1#euurwcrex@f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DEBUG")).lower()== "true"
+DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower()== "true"
 
 print("DEBUG" , DEBUG , type(DEBUG))
 
