d5895f529c232dec104e90786dc6351af26e6651
kimutai <kevinkogo@gmail.com>
2024-10-22 00:24:53 +0300

    created the django environment variables using python decouple

--- src/SaaSApp/settings.py
+++ src/SaaSApp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qh(ikiqv668v#78n5nk1o73qkbh6!_6h__w$g1#euurwcrex@f'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower()== "true"
-
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower()== "true"
+DEBUG = config("DJANGO_DEBUG",cast=bool)
 print("DEBUG" , DEBUG , type(DEBUG))
 
 ALLOWED_HOSTS = [
